0426779a4dafb92fde82a90bf37122be89f46baa
UttamJain185 <104371490+UttamJain185@users.noreply.github.com>
2022-06-15 22:26:43 +0530

    Add files via upload

---
+++ Assingnment_5_output_21107011.py
@@ -0,0 +1,126 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:\Users\AdminWS\.spyder-py3\untitled4.py ==============
+QUESTION No.1:
+Enter a string-:jdbvaklwe
+Traceback (most recent call last):
+  File "C:\Users\AdminWS\.spyder-py3\untitled4.py", line 10, in <module>
+    string2 = i + string2
+NameError: name 'string2' is not defined. Did you mean: 'string1'?
+
+============== RESTART: C:\Users\AdminWS\.spyder-py3\untitled4.py ==============
+QUESTION No.1:
+Enter a string-:jsdja
+The reversed string is :  ajdsj 
+
+QUESTION No.2:
+Enter your first no.-:24
+Enter your second no.-:49
+Enter the number which you will divide on entire range-:5
+All numbers in range (24, 49) which are divisible by 5 are as follows-:
+25
+30
+35
+40
+45
+
+QUESTION No.3:
+Enter your first side:-4
+Enter your second side:-7
+Enter your third side:-9
+Area is equal to :- 13.416407864998739
+
+QUESTION No.4:
+The given pattern is as follows-:
+*
+
+**
+
+***
+
+****
+
+*****
+
+*****
+
+****
+
+***
+
+**
+
+*
+
+QUESTION No.5:
+Enter the number of rows: 9
+A
+
+BC
+
+DEF
+
+GHIJ
+
+KLMNO
+
+PQRSTU
+
+VWXYZAB
+
+CDEFGHIJ
+
+KLMNOPQRS
+
+QUESTION No.6:
+24
+40
+The Prime Numbers in range  (24, 40) are as follows-: 
+29
+31
+37
+
+QUESTION No.7:
+Following are the numbers which are multiple of 7 and divisible by 11 in the range (1-500)-:
+77
+154
+231
+308
+385
+462
+
+QUESTION No.8:
+you have to Enter 10 integers of your choice-:
+Enter an integer-:4
+Enter an integer-:46
+Enter an integer-:46
+Enter an integer-:56
+Enter an integer-:43
+Enter an integer-:46
+Enter an integer-:12
+Enter an integer-:89
+Enter an integer-:1
+Enter an integer-:23
+This is the list of integers entered by the user-: [4, 46, 46, 56, 43, 46, 12, 89, 1, 23]
+positive numbers are as follows-:4,46,46,56,43,46,12,89,1,23,
+negative numbers are as follows-:
+odd numbers are as follows-:43,89,1,23,
+even numbers are as follows-:4,46,46,56,46,12,
+Enter the number you want to count in list1-:46
+The number - 46 , occurs 3 times in the list1
+
+QUESTION No.9:
+input the number of values in list : 6
+input the list values : 
+1
+2
+4
+4
+2
+5
+number of occurences : 
+The number  1  counts  1  in the list
+The number  2  counts  2  in the list
+The number  4  counts  2  in the list
+The number  5  counts  1  in the list
